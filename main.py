c2bcc11f656adb27564355488f1fb8c34f2e4d03
guilhermerocha9777-glitch <guilhermerocha9777@gmail.com>
2026-05-03 23:19:44 -0300

    primeiro commit do bot

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+print("TOKEN LIDO:", repr(TOKEN))  # linha de debug
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Bot funcionando!")
+
+print("Bot polling started")
+bot.polling()
